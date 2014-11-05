ab638d136da0da16f41dedebd2d8adc4a2288930
Joshua Romero <josh@gaugeinteractive.com>
2014-11-05 17:27:12 -0500

    Minor update to the chapter4.php file. Working on calling static methods and functions.

--- chapter4.php
+++ chapter4.php
@@ -4,9 +4,10 @@ class StaticExample
 {
 	static public $aNum = 0;
 	static public function sayHello() {
-		print "Hello!";
+		self::$aNum++;
+		print "Hello! (" . self::$aNum . ")";
 	}
 }
 
 print StaticExample::$aNum;
-StaticExample::sayHello();
+StaticExample::sayHello();`
